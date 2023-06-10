18a9cfae04f70e39e7c78d304fafaf2575ae092f
certainless <99538568+certainless@users.noreply.github.com>
2023-06-10 18:14:17 +0500

    Create IncFile1.h

---
+++ IncFile1.h
@@ -0,0 +1,33 @@
+/*
+ * IncFile1.h
+ *
+ * Created: 05/06/2023 05:40:43
+ *  Author: talha
+ */ 
+
+
+#ifndef INCFILE1_H_
+#define INCFILE1_H_
+
+//Define Map
+int pitchRow;
+int pitchCol;
+int goalCol;
+int goalRow;
+int startCol;
+int currCol;
+int currRow;
+int count;
+void avoid_obstacle();
+void avoid_obstacleROW1();
+void avoid_obstacleROW2();
+void obstacle_special();
+void navigate();
+void NextCol();
+void NextRow();
+void PrevRow();
+
+
+
+
+#endif /* INCFILE1_H_ */
